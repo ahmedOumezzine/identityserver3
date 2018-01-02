8acdd2cc8fccbdb4b225c1abaaa321da1be39fe2
ahmed Oumezzine <ahmedoumezzine@outlook.fr>
2018-01-02 14:12:35 -0800

    MVC OWIN Client (Implicit)

---
+++ MVC OWIN Client/Models/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_OWIN_Client.Models
+{
+    public static class Constants
+    {
+        public const string BaseAddress = "https://localhost:44333/core";
+
+        public const string AuthorizeEndpoint = BaseAddress + "/connect/authorize";
+        public const string LogoutEndpoint = BaseAddress + "/connect/endsession";
+        public const string TokenEndpoint = BaseAddress + "/connect/token";
+        public const string UserInfoEndpoint = BaseAddress + "/connect/userinfo";
+        public const string IdentityTokenValidationEndpoint = BaseAddress + "/connect/identitytokenvalidation";
+        public const string TokenRevocationEndpoint = BaseAddress + "/connect/revocation";
+
+        public const string AspNetWebApiSampleApi = "http://localhost:2727/";
+        public const string AspNetWebApiSampleApiUsingPoP = "http://localhost:46613/";
+    }
+}
